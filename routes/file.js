ea22ce1c703efa6ba9afcf1ee7ea5005aa126f00
iskander <irz2@tpu.ru>
2019-06-12 23:05:09 +0300

    file upload

--- routes/file.js
+++ routes/file.js
@@ -6,10 +6,10 @@ const fileModel = require("../models/File");
 
 const storage = multer.diskStorage({
 	destination: function(req, file, cb) {
-		cb(null, './uploads')
+		cb(null, './uploads/')
 	},
 	filename: function (req, file, cb) {
-		cb(null, Date.now() + file.originalname)
+		cb(null, file.originalname)
 	}
 });
 
