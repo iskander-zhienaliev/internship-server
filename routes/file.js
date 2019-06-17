a5fa5e5996b3d973301dc22a340495051390f6f2
iskander <irz2@tpu.ru>
2019-06-17 19:53:26 +0300

    file upload 5

--- routes/file.js
+++ routes/file.js
@@ -7,30 +7,36 @@ const path = require('path');
 
 const storage = multer.diskStorage({
 	destination: function(req, file, cb) {
-		cb(null, 'uploads/')
+		cb(null, 'uploads')
 	},
 	filename: function (req, file, cb) {
-		cb(null, file.fieldname + '-' + Date.now())
+		cb(null, file.originalname + '-' + Date.now())
 	}
 });
 
 const upload = multer({
 	storage: storage
-});
+}).single('file');
 
-router.post('/uploadfile', upload.single('fileData'), async (req, res, next) => {
-	console.log(req, 'll')
-	const newFile = await new fileModel({
-		fileName: req.body.fileName,
-		fileData: req.file.path
-	});
+router.post('/uploadfile', async (req, res, next) => {
+	upload(req, res, function(err) {
+		if (err instanceof multer.MulterError) {
+			return res.status(500).json(err)
+		} else if (err) {
+			return res.status(500).json(err)
+		}
+		const newFile = new fileModel({
+			fileName: req.body.fileName,
+			fileData: req.file.path
+		});
 
-	newFile.save().then((result) => {
-		res.status(200).json({
-			success: true,
-			result: result
-		})
-	}).catch(err => next(err))
+		newFile.save().then((result) => {
+			res.status(200).json({
+				success: true,
+				result: result
+			})
+		}).catch(err => next(err))
+	})
 });
 
 
