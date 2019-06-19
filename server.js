1ca7a7223ea8619796f9da0d77e7fbf0952dc45b
iskander <irz2@tpu.ru>
2019-06-19 06:45:15 +0300

    file upload 12

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
 const bodyParser = require('body-parser');
 const authRoute = require('./routes/auth');
 const postJob = require('./routes/postJob');
@@ -20,7 +21,31 @@ app.use('/auth', authRoute);
 app.use('/api', postJob);
 app.use('/update', updateModel);
 app.use('/get', getUser);
-app.use('/uploads', express.static(process.env.PWD+'static'));
+app.use('/static', express.static(process.env.PWD+'static'));
+
+const storage = multer.diskStorage({
+	destination: function(req, file, cb) {
+		cb(null, 'static')
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname + '-' + Date.now())
+	}
+});
+
+const upload = multer({
+	storage: storage
+});
+
+app.post('/static', async (req, res, next) => {
+	upload.single('file')(req, res, function(err) {
+		if (err instanceof multer.MulterError) {
+			return res.status(500).json(err)
+		} else if (err) {
+			return res.status(500).json(err)
+		}
+		return res.status(200).send(req.file)
+	})
+});
 
 app.use(function(req, res, next) {
 	let err = new Error("Not Found");
