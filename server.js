fd82e501e353372ae8d021b504cd148df7172cae
iskander <irz2@tpu.ru>
2019-06-20 19:04:21 +0300

    auth

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const getUser = require('./routes/getUser');
 const db = require("./models");
 
 const API_PORT = process.env.PORT || 3000;
+const dbRoute = "mongodb+srv://Slargar:sparksv@cluster0-razvx.mongodb.net/test?retryWrites=true\n";
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,31 +22,17 @@ app.use('/auth', authRoute);
 app.use('/api', postJob);
 app.use('/update', updateModel);
 app.use('/get', getUser);
-app.use('/static', express.static(process.env.PWD+'static'));
 
-const storage = multer.diskStorage({
-	destination: function(req, file, cb) {
-		cb(null, 'static')
-	},
-	filename: function (req, file, cb) {
-		cb(null, file.originalname + '-' + Date.now())
-	}
+const storage = require('multer-gridfs-storage')({
+	url: dbRoute
 });
 
-const upload = multer({
-	storage: storage
-});
+const upload = multer({storage: storage});
+const sUpload = upload.single('file');
 
-app.post('/static', async (req, res, next) => {
-	upload.single('file')(req, res, function(err) {
-		if (err instanceof multer.MulterError) {
-			return res.status(500).json(err)
-		} else if (err) {
-			return res.status(500).json(err)
-		}
-		return res.status(200).send(req.file)
-	})
-});
+app.post('/file', sUpload, (req, res, next) => {
+	console.log(req, res)
+})
 
 app.use(function(req, res, next) {
 	let err = new Error("Not Found");
